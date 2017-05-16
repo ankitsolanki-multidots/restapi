96fcb0ac8eaf574e537e32ced801fecbeb8bd357
Ankit Solanki <ankit.solanki@multidots.com>
2017-05-16 12:09:58 +0530

    cors remove

--- src/Module.php
+++ src/Module.php
@@ -20,11 +20,25 @@ class Module
      */
     public function onBootstrap(MvcEvent $e)
     {
-        // Set CORS headers to allow all requests
-        $headers = $e->getResponse()->getHeaders();
-        $headers->addHeaderLine('Access-Control-Allow-Origin: *');
-        $headers->addHeaderLine('Access-Control-Allow-Methods: PUT, GET, POST, PATCH, DELETE, OPTIONS');
-        $headers->addHeaderLine('Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept');
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            }
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            }
+            exit(0);
+        }
     }
 
 }
